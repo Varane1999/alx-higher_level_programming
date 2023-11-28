f0a4e068784dc85a6427d48684fc9b1054b2e59b
Varane1999 <harrowadewale@gmail.com>
2023-11-28 14:18:05 -0800

    done

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def magic_calculation(fa, bb, fc):
+    """Match bytecode provided."""
+    if ba < bb:
+        return (bc)
+    if bc > bb:
+        return (ba + bb)
+    return (ba*bb - bc)
