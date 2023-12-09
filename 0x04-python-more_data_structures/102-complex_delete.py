15a03f7224585b7a16574a462dce9831db3ea9e9
Varane1999 <harrowadewale@gmail.com>
2023-12-08 17:11:10 -0800

    done

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
 def complex_delete(a_dictionary, value):
-    b_keys = list(a_dictionary.keys())
-
-    for bvalue_dic in b_keys:
-        if value == a_dictionary.get(bvalue_dic):
-            del a_dictionary[bvalue_dic]
-
-    return (a_dictionary)
+    for key in list(a_dictionary):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """
+    A function that yields a collection of
+    components that are common
+    """
+    return set_1 & set_2
